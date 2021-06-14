8e98eb144f6976589c6b050ea867cf2e82e902f2
Tyler Wilson <tjwilson.91@gmail.com>
2021-06-14 14:45:10 -0700

    Add project files.

---
+++ ColorSlider.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace WpfControls
+{
+	public class ColorSlider : ValueSlider
+	{
+		static ColorSlider()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(ColorSlider), new FrameworkPropertyMetadata(typeof(ColorSlider)));
+		}
+	}
+}
